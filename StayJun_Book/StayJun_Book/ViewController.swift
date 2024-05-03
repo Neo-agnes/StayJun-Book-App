7de2ed03517718e74134d87beea9aa20abf87619
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-05-03 17:54:03 +0900

    [Feat]기본설정

--- StayJun_Book/StayJun_Book/ViewController.swift
+++ StayJun_Book/StayJun_Book/ViewController.swift
@@ -11,8 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        
+        // UIView 생성
+        let myView = UIView()
+        myView.backgroundColor = .blue
+        
+        // UIView의 frame 설정 (x, y, width, height)
+        myView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+
+        
+        // 현재 ViewController의 view에 myView 추가
+        view.addSubview(myView)
+        
+        // UILabel 생성 및 설정
+        let myLabel = UILabel()
+        myLabel.text = "Hello, World!"
+        myLabel.textColor = .white
+        
+        // UILabel의 크기를 텍스트에 맞게 조절
+        myLabel.sizeToFit()
+        
+        // UILabel을 myView의 중앙에 위치시키기
+        myLabel.center = CGPoint(x: myView.frame.size.width  / 2,
+                                 y: myView.frame.size.height / 2)
+        
+        // myView에 myLabel 추가
+        myView.addSubview(myLabel)
     }
-
 }
-
