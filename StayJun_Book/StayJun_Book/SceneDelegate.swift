8001def2b4a9879e7d689a21bbcdd4f0dea4f5f5
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-05-03 23:48:11 +0900

    [Feat]Tabbar and Search Set

--- StayJun_Book/StayJun_Book/SceneDelegate.swift
+++ StayJun_Book/StayJun_Book/SceneDelegate.swift
@@ -12,14 +12,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
+        // MARK: - 컨트롤러들의 장면 컨트롤
         guard let windowScene = (scene as? UIWindowScene) else { return }
-//        window = UIWindow(windowScene: windowScene)
-//        window?.rootViewController = ViewController()
-//        window?.makeKeyAndVisible()
         
         // 탭 바 컨트롤러 생성
         let tabBarController = UITabBarController()
-        
         
         // 각 탭에 해당하는 뷰 컨트롤러 생성
         let searchViewController = SearchViewController() // 책 검색 화면
@@ -29,37 +26,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         tabBarController.viewControllers = [searchViewController, savedBooksViewController]
         
         // 윈도우 생성 및 루트 뷰 컨트롤러로 탭 바 컨트롤러 설정
-        tabBarController.tabBarItem.image
-//        window = UIWindow(frame: UIScreen.main.bounds)
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
-
-    
-//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//
-//        // 탭 바 컨트롤러 생성
-//        let tabBarController = UITabBarController()
-//
-//        // 첫 번째 탭: 책 검색 화면
-//        let searchViewController = SearchViewController()
-//        searchViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-//
-//        // 두 번째 탭: 담은 책 리스트 화면
-//        let savedBooksViewController = SavedBooksViewController()
-//        savedBooksViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-//
-//        // 탭 바 컨트롤러에 뷰 컨트롤러들 추가
-//        tabBarController.viewControllers = [searchViewController, savedBooksViewController]
-//
-//        // 윈도우 생성 및 루트 뷰 컨트롤러 설정
-//        let window = UIWindow(windowScene: windowScene)
-//        window.rootViewController = tabBarController // 탭 바 컨트롤러를 루트로 설정
-//        self.window = window
-//        window.makeKeyAndVisible()
-//    }
     
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- StayJun_Book/StayJun_Book/ViewController.swift
+++ StayJun_Book/StayJun_Book/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
         
         // UIView의 frame 설정 (x, y, width, height)
         myView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
-
         
         // 현재 ViewController의 view에 myView 추가
         view.addSubview(myView)
@@ -32,8 +31,8 @@ class ViewController: UIViewController {
         myLabel.sizeToFit()
         
         // UILabel을 myView의 중앙에 위치시키기
-        myLabel.center = CGPoint(x: myView.frame.size.width  / 2,
-                                 y: myView.frame.size.height / 2)
+        myLabel.center = CGPoint (x: myView.frame.size.width  / 2,
+                                  y: myView.frame.size.height / 2)
         
         // myView에 myLabel 추가
         myView.addSubview(myLabel)
