7ce56a9bf638fb0221270759ac517843ea2598fd
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-05-03 15:38:37 +0900

    [Feat] UIcode setting

---
+++ StayJun_Book/StayJun_Book/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StayJun_Book
+//
+//  Created by Developer_P on 5/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+}
+
